bc101704437dca5f62a36dfee6c36aa35c1bc6a5
Bob Kwiencien <bob.kwiencien@hyperproductive.com>
2017-07-27 11:33:55 -0400

    Jul27-11:33

--- game.js
+++ game.js
@@ -2,13 +2,15 @@ var ComputerWins = 0;
 var Userins = 0;
 var Ties    = 0;
 function rock() {
-   return("R");
    console.log("R");
+   return("R");
 }
 function paper(){
+  console.log("P");
   return("P");
 }
 function scissors(){
+ console.log("S");
  return("S");
 }
 
