f1623b3e5e3f7ecfcde77b929ab8bc006d30dd9e
Bob Kwiencien <bob.kwiencien@hyperproductive.com>
2017-07-27 12:41:55 -0400

    Jul27-12:41

--- game.js
+++ game.js
@@ -6,9 +6,9 @@ var computerchoice="";
 var result;
    console.log("R");
    updateYouChoose("R");
-   computerchoice=computerChose();
+   computerchoice = computerChose();
+   updatecomputerChoose(computerchoice);
    result = "R" + computerchoice;
-   console.log(result);
    return("R");
 }
 function paper(){
@@ -24,15 +24,26 @@ function scissors(){
 function computerChose(){
 var tempnum = 0;
 var gener   = 0;
+var compchoice = " ";
 tempnum = Math.round(Math.random()*10)+1;
 gener = tempnum % 3;
-if (gener == 0) return("R");
-if (gener == 1) return("P");
-if (gener == 2) return("S");
+if (gener == 0){
+ return("R");
+}
+if (gener == 1) {
+ return("P");
+}
+if (gener == 2){
+ return("S");
+}
 }
 function updateYouChoose(x) {
 var YourChoice = x;
 var element = document.getElementById("youchoose");
 element.innerHTML = "You Chose: " + x;
 console.log(element);
+}
+function updatecomputerChoose(x) {
+var element = document.getElementById("computerchose");
+element.innerHTML = "Computer choce: " + x;
 }
