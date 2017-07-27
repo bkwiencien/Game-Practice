ffa31e9e91acccecb989930e1aa6efe9782b6c08
Bob Kwiencien <bob.kwiencien@hyperproductive.com>
2017-07-27 11:47:21 -0400

    Jul27-11:47

--- game.js
+++ game.js
@@ -2,7 +2,12 @@ var ComputerWins = 0;
 var Userins = 0;
 var Ties    = 0;
 function rock() {
+var computerchoice="";
+var result;
    console.log("R");
+   computerchoice=computerChose();
+   result = "R" + computerchoice;
+   console.log(result);
    return("R");
 }
 function paper(){
@@ -13,4 +18,14 @@ function scissors(){
  console.log("S");
  return("S");
 }
+function computerChose(){
+var tempnum = 0;
+var gener   = 0;
+tempnum = Math.round(Math.random()*10)+1;
+gener = tempnum % 3;
+if (gener == 0) return("R");
+if (gener == 1) return("P");
+if (gener == 2) return("S");
+}
+
 
